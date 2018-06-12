a476636036ad9d22a841f5396d12ca07efc3fd5e
LeftHandFreeStudio <kawka.maciej.93@gmail.com>
2018-06-12 13:16:33 +0200

    basic structure added

---
+++ src/main/java/com/lefthandfreestudio/headsTails/HeadsTailsApp.java
@@ -0,0 +1,11 @@
+package com.lefthandfreestudio.headsTails;
+import java.util.Scanner;
+
+public class HeadsTailsApp{
+    public static void main( String[] args ){
+		System.out.println("Do you want to play?")
+		Scanner playerCommandsReader = new Scanner(System.in);
+		String answer = playerCommandsReader.nextLine();
+		System.out.println(answer)
+    }
+}
