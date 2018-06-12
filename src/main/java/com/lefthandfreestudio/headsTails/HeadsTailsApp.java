2d708ac81c5b0d76a3c766a80b54053625e6df5b
LeftHandFreeStudio <kawka.maciej.93@gmail.com>
2018-06-12 13:23:46 +0200

    corrected lack of semicolon

--- src/main/java/com/lefthandfreestudio/headsTails/HeadsTailsApp.java
+++ src/main/java/com/lefthandfreestudio/headsTails/HeadsTailsApp.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class HeadsTailsApp{
     public static void main( String[] args ){
-		System.out.println("Do you want to play?")
+		System.out.println("Do you want to play?");
 		Scanner playerCommandsReader = new Scanner(System.in);
 		String answer = playerCommandsReader.nextLine();
 		System.out.println(answer);
